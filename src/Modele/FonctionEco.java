1150624079a086e3132de2499d579407ee7d4884
Martin Niombela <niombela.martin@gmail.com>
2019-04-04 13:44:56 +0000

    FonctionEco.java édité en ligne via Bitbucket

--- src/Modele/FonctionEco.java
+++ src/Modele/FonctionEco.java
@@ -7,20 +7,34 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ * <h1><i>FonctionEco</i></h1>
+ * <h2><code>public FonctionEco</code></h2>
+ * <p>Cette classe imite le comportement mathématique d'une fonction économique de simplexe.</p>
+ *
+ * @see Simplexe
+ */
 public class FonctionEco implements Serializable{
 	Map monomes;
 	
 	/**
-	 * Construit un objet FonctionEco contenant une HashMap
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+   	 * <h1><i>FonctionEco</i></h1>
+ 	 * <h2><code>public FonctionEco()</code></h2>
+	 * <p>Construit un objet <code>FonctionEco</code> contenant une <code>HashMap</code>.</p>
 	 */
 	public FonctionEco() {
 		monomes = new HashMap(); //changer en map
 	}
 	
 	/**
-	 * Construit un objet FonctionEco � partir d'un autre objet FonctionEco donn� en param�tre et copie son champs Map monomes
-	 * dans le champs Map monomes du nouvel objet � construire
-	 * @param FonctionEco fonctionEco
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+  	 * <h1><i>FonctionEco</i></h1>
+ 	 * <h2><code>public FonctionEco(FonctionEco fonctionEco)</code></h2>
+	 * <p>Construit un objet <code>FonctionEco</code> à partir d'un autre objet <code>FonctionEco</code> donné en paramètre et copie son champ <code>Map</code> <i>monomes</i>
+	 * dans le champ <code>Map</code> <i>monomes</i> du nouvel objet à construire.</p>
+	 * @param fonctionEco : (FonctionEco)
 	 */
 	public FonctionEco(FonctionEco fonctionEco) {
 		monomes = new HashMap();
@@ -29,17 +43,26 @@ public class FonctionEco implements Serializable{
 			this.ajouterMonome(new Monome((Monome)fonctionEco.monomes.get(cle)));;
 		}
 	}
+	
 	/**
-	 * Ajoute un Monome � la Map de monome de this
-	 * @param Monome m
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>ajouterMonome</i></h1>
+ 	 * <h2><code>public void ajouterMonome(Monome m)</code></h2>
+	 * <p>Ajoute un <code>Monome</code> à la <code>Map</code> de <i>monome</i> de <i><b>this</b></i>.</p>
+	 * @param m : (Monome)
+	 * 
+	 * @see Monome
 	 */
 	public void ajouterMonome(Monome m) {
 		monomes.put(m.getInconnue(), m); // transformer pour que �a s'adapte � la map
 	}
 	
 	/**
-	 * Renvoie une chaine de caract�re correspondant � la fonction �conomique
-	 *@return String chaineFinale
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>toString</i></h1>
+ 	 * <h2><code>public String toString()</code></h2>
+	 * <p>Renvoie une chaine de caractère correspondant à la fonction économique.</p>
+	 * @return chaineFinale : (String)
 	 */
 	public String toString() {
 		String chaineFinale = new String();
@@ -61,10 +84,17 @@ public class FonctionEco implements Serializable{
 
 	}
 	/**
-	 * Echange la variable "inconnue" de la fonction �conomique avec une contrainte explicite, puis simplifie l'expression en additionnant
-	 * les monomes de m�me inconnue.
-	 * @param ContrainteExplicite ce
-	 * @param String inconnue
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>echanger</i></h1>
+ 	 * <h2><code>public void echanger(ContrainteExplicite ce, String inconnue)</code></h2>
+	 * <p>Echange la variable "inconnue" de la fonction économique avec une contrainte explicite, puis simplifie l'expression en additionnant
+	 * les monomes de même inconnue.</p>
+	 * @param ce : (ContrainteExplicite)
+	 * @param inconnue : (String)
+	 * 
+	 * @see ContrainteExplicite
+	 * @see Monome#getCoefficient()
+	 * @see Fraction#FMultiplication(Fraction)
 	 */
 	public void echanger(ContrainteExplicite ce, String inconnue) {
 		Monome aEchanger = ((Monome)monomes.get(inconnue));
@@ -85,9 +115,15 @@ public class FonctionEco implements Serializable{
 		}
 		
 	}
+	
 	/**
-	 * Renvoie le Monome de la fonction �conomique ayant la valeur de coefficient maximale 
-	 * @return String res
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>monomeCoeffMax</i></h1>
+ 	 * <h2><code>public String monomeCoeffMax()</code></h2>
+	 * <p>Renvoie le <code>Monome</code> de la fonction économique ayant la valeur de coefficient maximale.</p>
+	 * @return res : (String)
+	 * 
+	 * @see Monome#getCoefficient()
 	 */
 	public String monomeCoeffMax() {
 		Fraction max = new Fraction(0);
@@ -104,9 +140,15 @@ public class FonctionEco implements Serializable{
 		return res;
 		
 	}
+	
 	/**
-	 * Renvoie le champs monomes (Map) de this
-	 * @return Map monomes
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>getMonomes</i></h1>
+ 	 * <h2><code>public Map getMonomes()</code></h2>
+	 * <p>Renvoie le champ <i>monomes</i> de <i><b>this</b></i>.</p>
+	 * @return monomes : (Map)
+	 * 
+	 * @see Monome
 	 */
 	public Map getMonomes() {
 		return monomes;
