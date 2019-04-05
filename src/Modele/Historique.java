387e00e4e08ed946f6cd8055fc30a60715d515a7
Martin Niombela <niombela.martin@gmail.com>
2019-04-05 15:46:29 +0000

    Historique.java édité en ligne via Bitbucket

--- src/Modele/Historique.java
+++ src/Modele/Historique.java
@@ -5,28 +5,46 @@ import java.util.LinkedList;
 
 import Modele.Simplexe;
 
+/**
+ * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ * <h1><i>Historique</i></h1>
+ * <h2><code>public class Historique implements Serializable</code></h2>
+ * <p>Cette classe contient les différents <i>dictionnaires</i> du simplexe en cours : <b>c'est une liste de <code>Simplexe</code></b>.</p>
+ */
 public class Historique implements Serializable{
 	
 	private LinkedList <Simplexe> listeSimplexe;
 	
 	/**
-	 * Renvoie le champs LinkedList<Simplexe> listeSimplexe de this
-	 * @return LinkedList<Simplexe> listeSimplexe
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+	 * <h1><i>getListeSimplexe</i></h1>
+ 	 * <h2><code>public LinkedList\<Simplexe\> getListeSimplexe()</code></h2>
+	 * <p>Renvoie le champ <i>listeSimplexe</i> de <i><b>this</b></i>, qui est une liste de Simplexe.</p>
+	 * @return listeSimplexe : (LinkedList)
+	 *
+	 * @see Simplexe
 	 */
 	public LinkedList<Simplexe> getListeSimplexe() {
 		return listeSimplexe;
 	}
+	
 	/**
-	 * Ajoute un Simplexe à la LinkedList<Simplexe> de this
-	 * @param Simplexe simplexe
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>add</i></h1>
+	 * <h2><code>public void add(Simplexe simplexe)</code></h2>
+	 * <p>Ajoute un <code>Simplexe</code> à la <i>listeSimplexe</i> de <i><b>this</b></i>.</p>
+	 * @param simplexe : (Simplexe)
 	 */
 	public void add(Simplexe simplexe) {
 		listeSimplexe.add(simplexe);
 	}
 	
 	/**
-	 * Parcourt la liste de Simplexe et les ajoute à la String chaineFinale
-	 *@return String chaineFinale
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>toString</i></h1>
+	 * <h2><code>public String toString()</code></h2>
+	 * <p>Parcourt la <i>listeSimplexe</i> de <i><b>this</b></i> et les renvoie sous forme de chaîne de caractère.</p>
+	 * @return chaineFinale : (String)
 	 */
 	public String toString() {
 		String chaineFinale= new String();
@@ -37,34 +55,47 @@ public class Historique implements Serializable{
 	}
 
 	/**
-	 * Défini le champ listeSimplexe de this avec la LinkedList<Simplexe> fournie en paramètre
-	 * @param LinkedList<Simplexe>listeSimplexe
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>setListeSimplexe(LinkedList\<Simplexe\> listeSimplexe)</i></h1>
+	 * <h2><code>public void add(Simplexe simplexe)</code></h2>
+	 * <p>Défini le champ <i>listeSimplexe</i> de <i><b>this</b</i> avec la <code>LinkedList\<Simplexe\></code> fournie en paramètre.</p>
+	 * @param listeSimplexe : (LinkedList\<Simplexe\>)
 	 */
 	public void setListeSimplexe(LinkedList<Simplexe> listeSimplexe) {
 		this.listeSimplexe = listeSimplexe;
 	}
 	
 	/**
-	 * Supprime le dernier Simplexe de la liste de Simplexe listeSimplexe (si il y a plus d'un Simplexe)
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>etapePrecedente()</i></h1>
+	 * <h2><code>public void void etapePrecedente()</code></h2>
+	 * <p>Supprime le dernier simplexe de la <i>listeSimplexe</i> de </i><b>this</b></i> (si il y a plus d'un simplexe).</p>
+	 *
+	 * @see Simplexe
 	 */
 	public void etapePrecedente() {
 		if(listeSimplexe.size()>1) {
 			listeSimplexe.removeLast();
 		}
-		
 	}
 	
 	/**
-	 * Construit un objet Historique à partir d'une liste de Simplexe LinkedList<Simplexe>
-	 * @param LinkedList<Simplexe>liste
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>Historique(LinkedList\<Simplexe\> liste)</i></h1>
+	 * <h2><code>public Historique(LinkedList\<Simplexe\> liste)</code></h2>
+	 * <p>Construit un objet <code>Historique</code> à partir d'une liste de <code>Simplexe</code> donnée en paramètre.</p>
+	 * @param liste : (LinkedList\<Simplexe\>)
 	 */
 	public Historique(LinkedList<Simplexe> liste) {
 		listeSimplexe = liste;
 	}
 	
 	/**
-	 * Construit un objet Historique à partir d'un autre objet Historique donné en paramètre
-	 * @param Historique histo
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>Historique</i></h1>
+	 * <h2><code>public Historique(Historique histo)</code></h2>
+	 * </p>Construit un objet <code>Historique</code> à partir d'un autre objet <code>Historique</code> donné en paramètre.
+	 * @param histo : (Historique)
 	 */
 	public Historique(Historique histo) {
 		listeSimplexe=new LinkedList();
@@ -75,7 +106,10 @@ public class Historique implements Serializable{
 	}
 	
 	/**
-	 * Construit un objet Historique contenant une LinkedList<Simplexe>
+	 * <style> body{ margin-left: 15px; margin-right: 15px; } </style>
+ 	 * <h1><i>Historique</i></h1>
+	 * <h2><code>public Historique()</code></h2>
+	 * <p>Construit un objet <code>Historique</code> contenant une liste de <code>Simplexe</code> vide.</p>
 	 */
 	public Historique() {
 		listeSimplexe = new LinkedList<Simplexe>();
